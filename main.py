f534c2892de502d1feceabd490ac5d72fe67b977
furuiyang <furuiyang0715@gmail.com>
2020-10-09 14:34:50 +0800

    部署

--- main.py
+++ main.py
@@ -31,11 +31,28 @@ class AntSpider(SpiderBase):
             print(code_str)
             JuChaoSearch(code_str).start()
 
+    def ding_inc_count(self):
+        self._spider_init()
+        sql = '''SELECT count(id) as inc_count FROM {} WHERE {} > date_sub(CURDATE(), interval 1 day);'''.format(
+            self.history_table_name, "AntTime")
+        inc_count = self.spider_client.select_one(sql).get("inc_count")
+        msg = '舆情猎手公告页面数据源【巨潮公告】今日截止目前\n按照公告发布时间 AntTime 新增 {}\n'.format(inc_count)
+
+        sql2 = '''SELECT count(id) as inc_count FROM {} WHERE {} > date_sub(CURDATE(), interval 1 day);'''.format(
+            self.history_table_name, "UPDATETIMEJZ")
+        inc_count2 = self.spider_client.select_one(sql2).get("inc_count")
+
+        msg += '按照插入时间 UPDATETIMEJZ 新增 {}\n'.format(inc_count2)
+        print(msg)
+        self.ding(msg)
+
 
 if __name__ == '__main__':
     # AntSpider().start()
+    AntSpider().ding_inc_count()
 
     schedule.every(20).minutes.do(AntSpider().start)
+    schedule.every(5).hours.do(AntSpider().ding_inc_count)
 
     while True:
         schedule.run_pending()
