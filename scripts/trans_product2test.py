97f53d21799aa714d6acb8fc0ec9717c14fe2381
furuiyang <furuiyang0715@gmail.com>
2020-10-16 14:20:31 +0800

    juchao_kuaixun 表的历史导入

--- scripts/trans_product2test.py
+++ scripts/trans_product2test.py
@@ -13,7 +13,7 @@ from base_spider import SpiderBase
 
 
 class DataTranser(SpiderBase):
-
+    """将测试库与线上的正式库保持一致 """
     def __init__(self):
         super(DataTranser, self).__init__()
         self.batch_number = 1000
@@ -22,7 +22,6 @@ class DataTranser(SpiderBase):
         """实时追踪 并保持两个数据库一致 """
         self._spider_init()
         self._test_init()
-
         end_time = datetime.datetime.now()
         start_time = end_time - datetime.timedelta(minutes=10)
 
@@ -37,6 +36,7 @@ class DataTranser(SpiderBase):
     def start(self):
         self._spider_init()
         self._test_init()
+
         # csql = '''
         #     CREATE TABLE IF NOT EXISTS `juchao_ant` (
         #       `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -62,9 +62,41 @@ class DataTranser(SpiderBase):
             print("sql: ", sql)
             datas = self.spider_client.select_all(sql)
             print('select count: ', len(datas))
+            if len(datas) == 0:
+                break
             save_count = self._batch_save(self.test_client, datas, 'juchao_ant',
                                           ['id',  'SecuCode', 'SecuAbbr', 'AntId', 'AntTime', 'AntTitle', 'AntDoc'])
             print("save count:", save_count)
+
+        # csql = '''
+        # CREATE TABLE IF NOT EXISTS `juchao_kuaixun` (
+        #   `id` int(11) NOT NULL AUTO_INCREMENT,
+        #   `code` varchar(8) DEFAULT NULL COMMENT '证券代码',
+        #   `name` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '证券简称',
+        #   `pub_date` datetime NOT NULL COMMENT '发布时间',
+        #   `title` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '资讯标题',
+        #   `type` varchar(16) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '资讯类别',
+        #   `link` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '公告详情页链接',
+        #   `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
+        #   `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        #   PRIMARY KEY (`id`),
+        #   UNIQUE KEY `date_title` (`pub_date`,`title`),
+        #   KEY `pub_date` (`pub_date`),
+        #   KEY `update_time` (`UPDATETIMEJZ`)
+        # ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='巨潮快讯';
+        # '''
+        # self.test_client.insert(csql)
+
+        for start in range(20):
+            sql = '''select * from juchao_kuaixun limit {}, {}; '''.format(start*self.batch_number, self.batch_number)
+            print(sql)
+            datas = self.spider_client.select_all(sql)
+            print("select count:", len(datas))
+            if len(datas) == 0:
+                break
+            save_count = self._batch_save(self.test_client, datas, 'juchao_kuaixun',
+                                          ['id', 'code', 'name', 'pub_date', 'title', 'type', 'link'])
+            print("save count: ", save_count)
 
 
 def task():
