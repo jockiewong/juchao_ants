59aacec34b8ac0059e41904768c6a5d5315fe34a
furuiyang <furuiyang0715@gmail.com>
2020-10-27 18:45:38 +0800

    Little

--- api_tests/news_gener_demo.py
+++ api_tests/news_gener_demo.py
@@ -1,3 +1,11 @@
 # 接新闻数据源 -->  news_base_tonglian(良哥) --> dc_ann_event_source_news_detail
 
 # 新闻先直接接通联库的数据
+from base_spider import SpiderBase
+
+
+class NewsGenerator(SpiderBase):
+    # vnews_content_v1  标题
+    # vnews_body_v1  内容
+
+    pass
