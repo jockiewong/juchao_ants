74944556f09321498e1ae325272a15a39360fb5a
furuiyang <furuiyang0715@gmail.com>
2020-10-27 17:17:04 +0800

    add next

---
+++ api_tests/guba_gener_demo.py
@@ -0,0 +1,4 @@
+# 接股吧数据源 --> guba_base(敏仪) --> dc_ann_event_source_guba_detail
+
+# guba_base 等敏仪灌入数据
+
---
+++ api_tests/news_gener_demo.py
@@ -0,0 +1,3 @@
+# 接新闻数据源 -->  news_base_tonglian(良哥) --> dc_ann_event_source_news_detail
+
+# 新闻先直接接通联库的数据
