dbce3811ec65d0b836b0b515c15fadf94faa733f
furuiyang <furuiyang0715@gmail.com>
2020-10-27 10:40:26 +0800

    get table max id

--- scripts/add_prefix.py
+++ scripts/add_prefix.py
@@ -7,3 +7,27 @@
 # max_id = 'select max(id) from announcement_base; '
 # for start in range(max_id//100000 + 1):
 #     sql = 'update xxx set where id >= {} and id <= {}...'.format(start, start*10000)
+from base_spider import SpiderBase
+
+
+class PrefixAdder(SpiderBase):
+    def __init__(self):
+        super(PrefixAdder, self).__init__()
+
+    def get_table_max_id(self, table_name: str):
+        self._tonglian_init()
+        sql = '''select max(id) as max_id from {}; '''.format(table_name)
+        max_id = self.tonglian_client.select_one(sql).get("max_id")
+        return max_id
+
+    def launch(self):
+        max_id = self.get_table_max_id("announcement_base")
+
+
+
+
+        pass
+
+
+if __name__ == '__main__':
+    PrefixAdder().launch()
