1ef90056daf86167112a0112962745da1a6b7dde
furuiyang <furuiyang0715@gmail.com>
2020-10-27 16:28:51 +0800

    add todos

---
+++ tasks_collection.py
@@ -0,0 +1,29 @@
+'''
+docker build -f Dockerfile -t registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/juchao_base:v1 .
+docker push registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/juchao_base:v1
+sudo docker pull registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/juchao_base:v1
+'''
+
+
+'''
+sudo docker run --log-opt max-size=10m --log-opt max-file=3 \
+-itd --name jclive --env LOCAL=1 --env VPN=1 \
+registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/juchao_base:v1 \
+python livenews/single_spider.py
+'''
+
+
+'''
+sudo docker run --log-opt max-size=10m --log-opt max-file=3 \
+-itd --name ant --env LOCAL=0 \
+registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/juchao_base:v1 \
+python history_ant/his_task.py
+'''
+
+
+''' 
+sudo docker run --log-opt max-size=10m --log-opt max-file=3 \
+-itd --name ant_merge --env LOCAL=1  \
+registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/juchao_base:v1 \
+python scripts/trans_2tl.py
+'''
