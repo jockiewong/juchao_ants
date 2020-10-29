012accb4e54373e623f513df7afdf8f735fca0bc
furuiyang <furuiyang0715@gmail.com>
2020-10-29 15:48:53 +0800

    adding

--- api_tests/guba_gener_demo.py
+++ api_tests/guba_gener_demo.py
@@ -36,7 +36,33 @@ class GubaGenerator(SpiderBase):
       KEY `k1` (`SecuCode`,`PubDatetime`,`AuthorType`,`UpdateTime`)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=810001 COMMENT='股吧主贴表';
 
+    CREATE TABLE `dc_ann_event_source_guba_detail` (
+      `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+      `GubaID` bigint(20) NOT NULL COMMENT '股吧主表ID',
+      `PubTime` datetime NOT NULL COMMENT '发布时间（精确到秒）',
+      `Title` varchar(500) DEFAULT NULL COMMENT '标题',
+      `Website` varchar(1000) DEFAULT NULL COMMENT '网址',
+      `SecuCode` varchar(20) DEFAULT NULL COMMENT '股票代码',
+      `EventCode` varchar(20) DEFAULT NULL COMMENT '事件代码',
+      `EventName` varchar(1000) DEFAULT NULL COMMENT '事件名称',
+      `Position` tinyint(4) NOT NULL COMMENT '提及位置：1-标题,2-内容',
+      `IsValid` tinyint(4) NOT NULL DEFAULT 1 COMMENT '是否有效',
+      `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+      `UpdateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+      PRIMARY KEY (`id`),
+      UNIQUE KEY `un1` (`GubaID`,`SecuCode`,`EventCode`),
+      KEY `k1` (`GubaID`,`PubTime`,`SecuCode`,`EventCode`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='公告舆情事件明细-股吧源' ;
     '''
 
-    pass
+    def __init__(self):
+        super(GubaGenerator, self).__init__()
+        self.source_table = 'guba_base'
+        self.target_table = 'dc_ann_event_source_guba_detail'
+
+    def launch(self):
+        self._yuqing_init()
+        sql = ''''''
+
+        pass
 
