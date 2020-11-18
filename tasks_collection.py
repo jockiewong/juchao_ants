647c30a405e59e12363862a2e17bccb09b120c34
furuiyang <furuiyang0715@gmail.com>
2020-11-18 16:39:02 +0800

    middle ann

--- tasks_collection.py
+++ tasks_collection.py
@@ -47,7 +47,7 @@ python middle_tables/news_gener_demo.py
 sudo docker run --log-opt max-size=10m --log-opt max-file=3 \
 -itd --name ann --env LOCAL=1  --env VPN=1 \
 registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/juchao_base:v1 \
-python middle_tables/ann_gener_demo.py
+python middle_tables/middle_dc_ann_event_source_ann_detail.py
 '''
 
 # TODO -v 做文件映射不重新 built + 做一个 python/shell 部署脚本
